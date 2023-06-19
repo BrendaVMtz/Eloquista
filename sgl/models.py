598c575adb527d942ca55a8a2e9cfc7e6cef8b67
brenda <bvergaram1500@alumno.ipn.mx>
2023-06-18 20:58:20 -0600

    Modelo de clases 1

--- sgl/models.py
+++ sgl/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from sgu.models import alumno
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+class Leccion(models.Model):
+    id = models.IntegerField(primary_key= True)
+    nivelRequerido = models.IntegerField
+    titulo = models.CharField
+
+class Ejercicio(models.Model):
+    idLeccion = models.ForeignKey(Leccion,on_delete=models.CASCADE)
+    tipo = models.CharField
+    enunciado = models.CharField
+    opcionesRespuesta = models.CharField
+    respuestaCorrecta = models.CharField
+
+class progresoEjercicios(models.Model):
+    idAlumno=models.ForeignKey(alumno,on_delete=models.CASCADE)
+    idEjercicio = models.ForeignKey(Ejercicio,on_delete=models.CASCADE)
+    ejercicioCompleto = models.BooleanField(default=False)
+    class Meta:
+        unique_together = ('idAlumno','idEjercicio')
+
+class progresoLecciones(models.Model):
+    idAlumno=models.ForeignKey(alumno,on_delete=models.CASCADE)
+    idLeccion= models.ForeignKey(Leccion,on_delete=models.CASCADE)
+    leccionCompleta = models.BooleanField(default=False)
+    class Meta:
+        unique_together = ('idAlumno','idLeccion')
+
+@receiver(post_save, sender=progresoEjercicios)
+def verificar_leccion_completa(sender, instance, **kwargs):
+    leccion = instance.idEjercicio.idLeccion
+    alumno = instance.idAlumno
+
+    if not Ejercicio.objects.filter(idLeccion=leccion, id__in=progresoEjercicios.objects.filter(idAlumno=alumno, ejercicioCompleto=False)).exists():
+        progreso = progresoLecciones.objects.get(idAlumno=alumno, idLeccion=leccion)
+        progreso.leccionCompleta = True
+        progreso.save()
--- sgu/models.py
+++ sgu/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class cuidador(models.Model):
+    nombreDeUsuario = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correoElectronico = models.CharField(max_length=100)
+    contrasena = models.CharField(max_length=50)
+
+class alumno(models.Model):
+    idCuidador = models.ForeignKey(cuidador,on_delete=models.CASCADE, default=0)
+    nombreDeUsuario = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    contrasena = models.CharField(max_length=50)
+    edad = models.IntegerField(
+    validators=[
+        MinValueValidator(0, message='El valor debe ser igual o mayor a 0.'),
+        MaxValueValidator(150, message='El valor debe ser igual o menor a 150.')
+    ]
+)
