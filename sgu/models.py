e89882711df8b2686b2a440c63cab5207521a573
brenda <bvergaram1500@alumno.ipn.mx>
2023-06-19 05:33:47 -0600

    base de datos 2

--- sgu/models.py
+++ sgu/models.py
@@ -3,22 +3,45 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
-class cuidador(models.Model):
-    nombreDeUsuario = models.CharField(max_length=50)
+class usuario(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    correoElectronico = models.CharField(max_length=100)
+    nombreDeUsuario = models.CharField(max_length=50, unique=True,primary_key=True)
     contrasena = models.CharField(max_length=50)
 
+
+class cuidador(models.Model):
+    nombreDeUsuario = models.OneToOneField(usuario,on_delete=models.CASCADE)
+    correoElectronico = models.CharField(max_length=100)
+
+class padre(cuidador):
+    calle = models.CharField(max_length=100)
+    numero = models.CharField(max_length=100)    
+    delegacion = models.CharField(max_length=100)    
+    telefono = models.PositiveIntegerField  
+
+class salud(cuidador):
+    cedula = models.CharField(max_length=100)
+    institucion = models.CharField(max_length=100)    
+    delegacion = models.CharField(max_length=100)    
+    telefono = models.PositiveIntegerField  
+
+class profesor(cuidador):
+    cedula = models.CharField(max_length=100)
+    institucion = models.CharField(max_length=100)    
+    delegacion = models.CharField(max_length=100)    
+    telefono = models.PositiveIntegerField    
+
 class alumno(models.Model):
-    idCuidador = models.ForeignKey(cuidador,on_delete=models.CASCADE, default=0)
-    nombreDeUsuario = models.CharField(max_length=50)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    contrasena = models.CharField(max_length=50)
+    #idCuidador = models.ForeignKey(cuidador,on_delete=models.CASCADE, related_name='idCuidador')
+    nombreDeUsuario = models.OneToOneField(usuario,on_delete=models.CASCADE)
     edad = models.IntegerField(
     validators=[
         MinValueValidator(0, message='El valor debe ser igual o mayor a 0.'),
         MaxValueValidator(150, message='El valor debe ser igual o menor a 150.')
     ]
 )
+
+class alumno_cuidador(models.Model):
+    alumno = models.ForeignKey(alumno,on_delete=models.SET_NULL,null=True)
+    cuidador = models.ForeignKey(cuidador,on_delete=models.CASCADE)
